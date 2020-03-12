ef4f86b623f6161faea666158b3502f95b6198c8
Marvin Reyes <reyemarv@gmail.com>
2020-03-12 19:56:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self.send("name=", attrtibutes_has[:name])
+    self.send("name=", attrtibutes_hash[:name])
       
   end
 
